1f452b4d40f0f4f740cb2516c479f4d1ae3ff7ce
Prashuk Jain <prashukjain7524@gmail.com>
2020-06-18 15:16:25 +0530

    commit

--- src/important/bulbstatus.java
+++ src/important/bulbstatus.java
@@ -1,10 +1,16 @@
 package important;
 
+import java.util.Scanner;
+
 public class bulbstatus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+for(int i=1;i*i<=n;i++) {
+	System.out.println(i*i);
+}
 	}
 
 }
